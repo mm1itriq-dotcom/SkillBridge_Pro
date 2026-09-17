9d373a91335006d36e0a93a3f736ff8be278d85c
mm1itriq-dotcom <mm1.itriq@gmail.com>
2026-09-17 17:05:24 +0300

    Refactor: Secure database credentials using dotenv

--- db.py
+++ db.py
@@ -1,6 +1,10 @@
+import os
 from sqlalchemy import create_engine, MetaData
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:123456@localhost/skillbridge"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:YOUR_PASSWORD@localhost/skillbridge")
 
 engine = create_engine(DATABASE_URL, echo=True)
 metadata = MetaData()
